30dfe33dfe745e6ba49f8c83733813ee79cbdd34
LucasDamascenoo <lucas.oliveira.damasceno@gmail.com>
2022-03-07 21:59:30 -0300

    Aqui estou mais um dia

---
+++ maratona-java/src/aula01Olamundo.java
@@ -0,0 +1,11 @@
+
+public class aula01Olamundo {
+
+  public static void main(String[] args) {
+    
+      String bts = "Bts é muito ruim, tem gente besta que ainda paga pra ver";
+
+      System.out.println(bts);
+  }
+
+}

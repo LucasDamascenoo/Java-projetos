5d1bedc7adb72bce2d25ea2afc85c210213cc262
LucasDamascenoo <lucas.oliveira.damasceno@gmail.com>
2022-03-09 15:11:19 -0300

    aprender nunca e demais, mas java e ossooooo

---
+++ maratona-java/introducao/aula07Arrays.java
@@ -0,0 +1,13 @@
+public class aula07Arrays {
+
+    public static void main(String[] args) {
+        //arrays são listas onde armazenamos diversos tipos de dados.
+
+        int [] idades = new int[3];
+        idades[0]=10;
+        idades[1]=10;
+        idades[2]=10;
+
+        System.out.println(idades[1]);
+    }
+}
---
+++ maratona-java/introducao/aula08ArraysMultimensionais.java
@@ -0,0 +1,5 @@
+public class aula08ArraysMultimensionais {
+    public static void main(String[] args) {
+
+    }
+}

d7770e21af5037c2770f349b3c3b703b23b84c44
LucasDamascenoo <lucas.oliveira.damasceno@gmail.com>
2022-03-08 23:54:17 -0300

    Aprendendo java para falar que ele me deu uma casa daqui 5 anos

---
+++ maratona-java/introducao/aula06LacosRepeticao02.java
@@ -0,0 +1,9 @@
+public class aula06LacosRepeticao02 {
+
+    public static void main(String[] args) {
+        for(int num =0;num <10;num++){
+            System.out.println(num);
+        }
+
+    }
+}
